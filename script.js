25ee38f6e708dfcccfc17b80d0551e0637be6d20
KJaeYeon <jaeyeon.k96@gmail.com>
2020-02-13 11:31:36 +0900

    angular first project app
    앵귤러 앱 - 1일차

---
+++ script.js
@@ -0,0 +1,25 @@
+// Code goes here
+(function(){
+  var app=angular.module('todoApp',[]);
+  
+  app.controller('TodoCtrl',['$scope',function($scope){
+    $scope.todos=[
+        {
+          title:'운동하기',
+          completed:true,
+          createdAt:Date.now()
+        },
+        {
+          title:'요리하기',
+          completed:false,
+          createdAt:Date.now()
+        },
+        {
+          title:'공부하기',
+          completed:false,
+          createdAt:Date.now()
+        }
+      ];
+  }]);
+})();
+
